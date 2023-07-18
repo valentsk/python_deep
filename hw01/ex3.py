56c85f304b7dc1a341895aa1cdf903a960216fe4
valentsk <skredovavalenti@mail.ru>
2023-07-18 17:00:16 +0300

    ex3

--- hw01/ex3.py
+++ hw01/ex3.py
@@ -2,3 +2,22 @@
 # Программа должна подсказывать «больше» или «меньше» после каждой попытки.
 # Для генерации случайного числа используйте код: from random import randint num = randint(LOWER_LIMIT, UPPER_LIMIT)
 
+from random import randint
+secret_number = randint(0, 10)
+attempt = 10
+inc_attempt = 1
+print('Я загадал число, у тебя 10 попыток его найти!')
+while attempt > 0:
+    print(f'Попытка №{inc_attempt}')
+    user_number = int(input("Введи число: "))
+    if user_number < secret_number:
+        print('Больше')
+    elif user_number > secret_number:
+        print('Меньше')
+    else:
+        print('Ты угадал!')
+        break
+    attempt -= 1
+    inc_attempt += 1
+if attempt == 0:
+    print('У тебя закончились попытки! Попробуй еще!')
