3e1e594090a20b14a0b48edc84b47fe574a77c7b
valentsk <skredovavalenti@mail.ru>
2023-08-20 14:19:17 +0300

    ex3

--- hw04/ex2.py
+++ hw04/ex2.py
@@ -1,3 +1,13 @@
-
 # Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь,
 # где ключ — значение переданного аргумента, а значение — имя аргумента. Если ключ не хешируем, используйте его строковое представление.
+
+def invers_try(**kwargs):
+    new_dict = {}
+    for key, val in kwargs.items():
+        try:
+            new_dict[val] = key
+        except:
+            new_dict[str(val)] = key
+    return new_dict
+
+print(invers_try(one=1, two = ['2'], three = (3,)))
