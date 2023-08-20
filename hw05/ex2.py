24f0e364d805a22a20bd670b6d4cd40bf564e313
valentsk <skredovavalenti@mail.ru>
2023-08-20 23:32:13 +0300

    ex2 ex3

--- hw05/ex2.py
+++ hw05/ex2.py
@@ -2,3 +2,10 @@
 # имена str, ставка int, премия str с указанием процентов вида «10.25%».
 # В результате получаем словарь с именем в качестве ключа и суммой премии в качестве значения.
 # Сумма рассчитывается как ставка умноженная на процент премии
+
+name_list = ['Петр', 'Иван', 'Мария']
+index_list = [10, 20, 30]
+bonus_list = ['10.25%', '15.3%', '20.9%']
+dict_var = {name_key: index * float(bonus[:-1]) / 100 for (name_key, index, bonus) in zip(name_list, index_list, bonus_list)}
+print(dict_var)
+# dict_var = {item[0]: item[1] * float(item[2][:-1]) / 100 for item in zip(name_list, index_list, bonus_list)}
--- hw05/ex3.py
+++ hw05/ex3.py
@@ -1,2 +1,11 @@
 # Создайте функцию генератор чисел Фибоначчи
 
+def gen_Fibo(number: int = 10):
+    first, second = 0, 1
+    while number > 0:
+        yield first
+        first, second = second, first + second
+        number -= 1
+
+for i in gen_Fibo():
+    print(i)
